588b5c2dfe447808e98cf66e800a0d3c08490deb
Nick Lindeberg <lindebergnick@gmail.com>
2018-08-23 14:53:46 -0600

    adds @article to controller/show

--- spec/features/user_sees_article_show_page_spec.rb
+++ spec/features/user_sees_article_show_page_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe "user sees one article" do
   describe "they link from the articles index" do
     it "displays information for one article" do
-      article_1 = Article.create!(title: "New Title", body: "New Body")
+      article = Article.create!(title: "New Title", body: "New Body")
 
       visit articles_path
 
-      clink_link article.title
+      click_link article.title
 
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
